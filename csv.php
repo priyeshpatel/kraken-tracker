eb3366bb46e5bcb62bd0dc6804b7fe4f24c34e8e
Priyesh Patel <priyesh@ukawala.com>
2013-08-19 17:52:45 +0100

    added date to csv

--- csv.php
+++ csv.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "http://habitat.habhub.org/habitat/_design/ept/_list/csv/payload_telemetry/payload_time?include_docs=true&startkey=[%2214f4eb90052b267e43ade2d4bfbfafff%22,1367494880]&endkey=[%2214f4eb90052b267e43ade2d4bfbfafff%22,[]]&fields=sentence_id,time,latitude,longitude,altitude,satellites,temperature_internal,battery,sentence_id,time,latitude,longitude,altitude,satellites,temperature_internal,battery");
+curl_setopt($curl, CURLOPT_URL, "http://habitat.habhub.org/habitat/_design/ept/_list/csv/payload_telemetry/payload_time?include_docs=true&startkey=[%2214f4eb90052b267e43ade2d4bfbfafff%22,1367494880]&endkey=[%2214f4eb90052b267e43ade2d4bfbfafff%22,[]]&fields=sentence_id,time,transmit_time,latitude,longitude,altitude,satellites,temperature_internal,battery");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($curl);
 curl_close($curl);
