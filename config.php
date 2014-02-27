772eee77ca229612525489323ca3048c2e9e1f22
Priyesh Patel <priyesh@ukawala.com>
2014-02-27 20:56:00 +0000

    changed config location

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://raw.github.com/danielsaul/kraken/tracker-config/config.json");
+curl_setopt($curl, CURLOPT_URL, "https://raw.github.com/priyeshpatel/kraken-tracker/config/config.json");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($curl);
 curl_close($curl);
